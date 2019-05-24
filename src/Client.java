985db64b164211a70268e802861627e4081ad7c3
Alexey Solovyev <solovevalex@gmail.com>
2019-05-24 20:21:14 +0300

    Переписал код класса Client (The Client class cod has been optimized).

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,23 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 
     public static void main(String[] args) {
 
+        try (Scanner scanner = new Scanner(System.in);
+             Socket socket = new Socket("localhost", 7777)) {
 
-        
+            String user = "Клиент";
+
+            UserHandler userHandler = new UserHandler(socket, scanner, user);
+            userHandler.ReciveMess();
+            userHandler.sendMess();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+
+        }
     }
 }
